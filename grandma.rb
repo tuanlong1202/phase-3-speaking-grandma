de6522c44323928d79787fe03213440bd6f3a71d
tuanlong1202 <tuanlong1202@gmail.com>
2021-07-22 11:14:45 -0400

    Phase 3 speaking to grandma

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,14 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma(speak)    
+    shout = speak.upcase
+    if speak != shout 
+        return "HUH?! SPEAK UP, SONNY!"
+    elsif speak == "I LOVE YOU GRANDMA!"
+        return "I LOVE YOU TOO PUMPKIN!"
+    elsif speak != "I LOVE YOU GRANDMA!"
+        return "NO, NOT SINCE 1938!"
+    end
+end
